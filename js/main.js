ce7d4521fedd8aa99ed898f0daa4b127cc68d964
Jeriah Bowers <jeriah97bowers@yahoo.com>
2020-04-05 16:17:51 -0400

    adding work portfolio section and stlyes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const navController = (function() {
+    const navigation = document.querySelector(".navigation");
+
+    const onScroll = () => {
+        if( window.scrollY > 50 ) {
+            navigation.style.background = 'rgba(0, 0, 0, 0.9)';
+        } else {
+            navigation.style.background = 'transparent';
+        }
+    };
+
+    window.addEventListener("scroll", onScroll);
+}());
+
+
 const headerController = (function() {
     const underlinedWord = document.querySelector(".heading span");
     
